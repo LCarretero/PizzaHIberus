4441f02f2fa15b741d7bf88571f807190e44938a
Lisardo Carretero Colmenar <lcarretero@hiberus.com>
2023-09-20 12:01:11 +0200

    las aplicaciones funcionan correctamente

--- CursoIntroduccionSpringCloud/gateway-server/src/main/java/com/hiberus/ApplicationGatewayServer.java
+++ CursoIntroduccionSpringCloud/gateway-server/src/main/java/com/hiberus/ApplicationGatewayServer.java
@@ -2,9 +2,10 @@ package com.hiberus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableEurekaClient
 public class ApplicationGatewayServer {
     public static void main(final String[] args) {
--- CursoIntroduccionSpringCloud/pizzas-read/src/main/java/com/hiberus/ApplicationPizzasRead.java
+++ CursoIntroduccionSpringCloud/pizzas-read/src/main/java/com/hiberus/ApplicationPizzasRead.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
+@EnableEurekaClient
 public class ApplicationPizzasRead {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationPizzasRead.class,args);
